120daafa850cb865acc60d3a1879e8e4f65aa497
tfkhdyt <tfkhdyt@pm.me>
2022-05-09 14:48:18 +0700

    add result padding y

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -30,47 +30,49 @@ function Result(): JSX.Element {
 
   return (
     <div className='p-4'>
-      <motion.div
-        variants={variants}
-        initial='initial'
-        animate='in'
-        transition={{
-          type: 'tween',
-          ease: 'easeInOut',
-          duration: 0.5,
-        }}
-        key={data?.content}
-        className='italic leading-relaxed'
-      >
-        {data ? (
-          data?.content
-        ) : (
-          <div className='space-y-3'>
-            <div className='h-4 w-full animate-pulse rounded bg-slate-200' />
-            <div className='h-4 w-5/6 animate-pulse rounded bg-slate-200' />
-            <div className='h-4 w-3/6 animate-pulse rounded bg-slate-200' />
-          </div>
-        )}
-      </motion.div>
-      <motion.span
-        variants={variants}
-        initial='initial'
-        animate='in'
-        key={data?.author}
-        transition={{
-          type: 'tween',
-          ease: 'easeInOut',
-          duration: 0.5,
-        }}
-        className='my-2 block text-sm font-bold tracking-tight'
-      >
-        {data ? (
-          `~ ${data?.author}`
-        ) : (
-          <div className='h-4 w-16 animate-pulse rounded bg-slate-400' />
-        )}
-      </motion.span>
-      <div className='flex items-stretch space-x-2 pt-2'>
+      <div className='py-6'>
+        <motion.div
+          variants={variants}
+          initial='initial'
+          animate='in'
+          transition={{
+            type: 'tween',
+            ease: 'easeInOut',
+            duration: 0.5,
+          }}
+          key={data?.content}
+          className='italic leading-relaxed'
+        >
+          {data ? (
+            data?.content
+          ) : (
+            <div className='space-y-3'>
+              <div className='h-4 w-full animate-pulse rounded bg-slate-200' />
+              <div className='h-4 w-5/6 animate-pulse rounded bg-slate-200' />
+              <div className='h-4 w-3/6 animate-pulse rounded bg-slate-200' />
+            </div>
+          )}
+        </motion.div>
+        <motion.span
+          variants={variants}
+          initial='initial'
+          animate='in'
+          key={data?.author}
+          transition={{
+            type: 'tween',
+            ease: 'easeInOut',
+            duration: 0.5,
+          }}
+          className='my-2 block text-sm font-bold tracking-tight'
+        >
+          {data ? (
+            `~ ${data?.author}`
+          ) : (
+            <div className='h-4 w-16 animate-pulse rounded bg-slate-400' />
+          )}
+        </motion.span>
+      </div>
+      <div className='flex items-stretch space-x-2'>
         <Button text='Generate' mutate={mutate} />
         <Select />
       </div>
